44221cdb40d74ad684f36333dfbf99dae13f5140
Hadar4476 <hadar4476@gmail.com>
2020-11-12 15:57:54 +0200

    version 1.0.0

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/api/auth', auth);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/client/dist/client'));
+  app.use(express.static('client/dist/client'));
   app.get('*', (req, res) => {
     res.sendFile(
       path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
