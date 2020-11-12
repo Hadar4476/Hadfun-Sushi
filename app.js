bf1df3c3282b7d344ee96a5d8efcc9356d2e19ef
Hadar4476 <hadar4476@gmail.com>
2020-11-12 15:57:22 +0200

    version 1.0.0

--- app.js
+++ app.js
@@ -35,9 +35,11 @@ app.use('/api/auth', auth);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/client/dist/'));
+  app.use(express.static('client/client/dist/client'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
+    );
   });
 }
 
